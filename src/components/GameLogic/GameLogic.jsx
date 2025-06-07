8dbffb137eaffaf55b6a011dbc1894f1b9f03d06
Denisa Polesná <polesnadenisa@gmail.com>
2025-06-07 12:22:22 +0200

    Assets moved from components, Add usePCKeyHandlers hook

--- src/components/GameLogic/GameLogic.jsx
+++ src/components/GameLogic/GameLogic.jsx
@@ -1,8 +1,11 @@
-import './GameLogic.css';
-import NavBar from '../UI/NavBarMenu/NavBar/NavBar';
-import Keyboard from '../Piano/Keyboard/Keyboard';
+import "./GameLogic.css";
+import NavBar from "../UI/NavBarMenu/NavBar/NavBar";
+import Keyboard from "../Piano/Keyboard/Keyboard";
+import usePCKeyHandlers from "../../hooks/usePCKeyHandlers";
 
 const GameLogic = () => {
+  usePCKeyHandlers(handleKeyDown, handleKeyUp);
+
   return (
     <div className="game-page">
       <div className="game-header">
