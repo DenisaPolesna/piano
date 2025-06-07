accb5fb146188d4bdb93f4c967c3cd4de15b4e94
Denisa Polesná <polesnadenisa@gmail.com>
2025-06-07 11:12:41 +0200

    Add PlayPauseBtn component

--- src/components/UI/NavBarMenu/NavBar/NavBar.jsx
+++ src/components/UI/NavBarMenu/NavBar/NavBar.jsx
@@ -1,10 +1,12 @@
 import "./NavBar.css";
 import HomeBtn from "../NavBarIcons/HomeBtn/HomeBtn";
+import PlayPauseBtn from "../NavBarIcons/PlayPauseBtn/PlayPauseBtn";
 
 const NavBar = (props) => {
   return (
     <nav className="game-nav">
       <HomeBtn className={"menu-icon"} />
+      <PlayPauseBtn />
     </nav>
   );
 };
---
+++ src/components/UI/NavBarMenu/NavBarIcons/PlayPauseBtn/PlayPauseBtn.jsx
@@ -0,0 +1,12 @@
+import playIcon from "./img/play.svg";
+import pauseIcon from "./img/pause.png";
+
+const PlayPauseBtn = () => {
+  return (
+    <>
+      <img src={playIcon} alt="play-icon" className="menu-icon" />
+    </>
+  );
+};
+
+export default PlayPauseBtn;
