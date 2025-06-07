428c6fa0777013b9d5933861d24691709a61e30b
Silvia Strazovcova <silviczka@yahoo.com>
2025-06-07 11:57:49 +0200

    keyboard visual

--- src/components/GameLogic/GameLogic.jsx
+++ src/components/GameLogic/GameLogic.jsx
@@ -1,11 +1,13 @@
-import NavBar from "../UI/NavBarMenu/NavBar/NavBar";
-import "./GameLogic.css";
+import './GameLogic.css';
+import NavBar from '../UI/NavBarMenu/NavBar/NavBar';
+import Keyboard from '../Piano/Keyboard/Keyboard';
 
 const GameLogic = () => {
   return (
     <div className="game-page">
       <div className="game-header">
         <NavBar />
+        <Keyboard />
       </div>
     </div>
   );
---
+++ src/components/Piano/Keyboard/Keyboard.jsx
@@ -0,0 +1,64 @@
+import './Keyboard.css';
+import Key from '../Key/Key';
+import { useState } from 'react';
+import { whiteKeys } from '../../assets/keys/whiteKeys';
+import { blackKeys } from '../../assets/Keys/blackKeys';
+
+const Keyboard = () => {
+  const [activeKeys, setActiveKeys] = useState(new Set()); // track multiple active keys
+
+  const playSound = (note) => {
+    const formattedNote = note.includes('#')
+      ? note.replace('#', 'Sharp')
+      : note;
+    const soundFile = `/assets/Sounds/${formattedNote}.mp3`;
+    console.log(soundFile);
+    const audio = new Audio(soundFile);
+    audio.play();
+
+    // add key to active keys set
+    setTimeout(() => {
+      setActiveKeys((prevActiveKeys) => new Set([...prevActiveKeys, note]));
+    }, 100); // 100ms
+
+    // stop the sound after it finishes
+    audio.onended = () => {
+      setActiveKeys((prevActiveKeys) => {
+        const newActiveKeys = new Set(prevActiveKeys);
+        newActiveKeys.delete(note); // remove the note when sound ends
+        return newActiveKeys;
+      });
+    };
+  };
+
+  const renderKey = ({ note }, type) => (
+    <Key key={`${type}-${note}`} note={note} type={type} />
+  );
+
+  return (
+    <div className="keyboard-wrapper ">
+      <div className="keyboard">
+        <div className="keyboard-white">
+          {whiteKeys.map((key) =>
+            renderKey(
+              {
+                note: key.note,
+              },
+              'white',
+            ),
+          )}
+        </div>
+        {blackKeys.map((key) =>
+          renderKey(
+            {
+              note: key.note,
+            },
+            'black',
+          ),
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Keyboard;
