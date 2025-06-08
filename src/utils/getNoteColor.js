1eba556eacad00d24ccb6cf25644e8a8ba95990b
Denisa Polesná <polesnadenisa@gmail.com>
2025-06-08 23:02:13 +0200

    Show score for sharp notes

--- src/utils/getNoteColor.js
+++ src/utils/getNoteColor.js
@@ -2,6 +2,9 @@ import whiteKeys from "../assets/keys/whiteKeys";
 import blackKeys from "../assets/keys/blackKeys";
 
 const getNoteColor = (note) => {
+  if (note.includes("Sharp")) {
+    note = note.replace("Sharp", "#");
+  }
   const key = [...whiteKeys, ...blackKeys].find((k) => k.note === note);
   return key ? key.color : null;
 };
